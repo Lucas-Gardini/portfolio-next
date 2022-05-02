589a126ebec88a4a54f57bc399a3773de1a4c4f5
lucas.dias <lucas.dias@integrativa.com.br>
2022-05-02 16:41:59 -0300

    added typescript + nextui

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from "react";
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: (<>{initialProps.styles}</>) as any,
+		};
+	}
+
+	render() {
+		return (
+			<Html lang="pt-br">
+				<Head>{CssBaseline.flush()}</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
